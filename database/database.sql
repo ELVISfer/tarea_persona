a22f8065bce7bb83736d5f9cfed6c2face2cab0a
ELVISfer <115681920+ELVISfer@users.noreply.github.com>
2023-10-16 23:38:45 -0600

    Update database.sql

--- database/database.sql
+++ database/database.sql
@@ -1,13 +1,5 @@
 -- Active: 1691546315151@@127.0.0.1@5432@tarea1@public
 
-create table
-    tbl_persona_de_Adm (
-        id serial PRIMARY key,
-        nombre varchar(200),
-        apellido varchar(200),
-        finish BOOLEAN DEFAULT false
-    );
-
 CREATE TABLE
     tbl_listado (
         id serial PRIMARY KEY,
